f9b26f56cf1943d92fac4ee2c5826be034c0e5aa
Ilya Tribusean <slash3b@gmail.com>
2023-02-15 00:52:10 +0200

    write request if fixed, somehow it was broken

--- packets/writereq.go
+++ packets/writereq.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"strings"
 
 	"tftp"
@@ -24,18 +25,18 @@ type WriteReq struct {
 	Mode     tftp.TransferMode
 }
 
-func (r *WriteReq) MarshalBinary() ([]byte, error) {
-	dataBytesLen := 2 + len(r.Filename) + 1 + len(r.Mode) + 1
+func (w *WriteReq) MarshalBinary() ([]byte, error) {
+	dataBytesLen := 2 + len(w.Filename) + 1 + len(w.Mode) + 1
 	data := make([]byte, 0, dataBytesLen)
 
 	buf := bytes.NewBuffer(data)
 
-	err := binary.Write(buf, binary.BigEndian, tftp.OpRRQ)
+	err := binary.Write(buf, binary.BigEndian, tftp.OpWRQ)
 	if err != nil {
 		return []byte{}, err
 	}
 
-	_, err = buf.WriteString(r.Filename)
+	_, err = buf.WriteString(w.Filename)
 	if err != nil {
 		return []byte{}, err
 	}
@@ -58,44 +59,46 @@ func (r *WriteReq) MarshalBinary() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func (q *WriteReq) UnmarshalBinary(p []byte) error {
-	r := bytes.NewBuffer(p)
+func (w *WriteReq) UnmarshalBinary(p []byte) error {
+	buf := bytes.NewBuffer(p)
 
 	var code tftp.OpCode
 
-	err := binary.Read(r, binary.BigEndian, &code) // read operation code
+	err := binary.Read(buf, binary.BigEndian, &code) // read operation code
 	if err != nil {
 		return err
 	}
 
-	if code != tftp.OpRRQ {
-		return errors.New("invalid RRQ")
+	if code != tftp.OpWRQ {
+		return fmt.Errorf("invalid write request, expected op code %d, received %d", tftp.OpWRQ, code)
 	}
 
-	q.Filename, err = r.ReadString(0) // read filename
+	w.Filename, err = buf.ReadString(0) // read filename
 	if err != nil {
-		return errors.New("invalid RRQ")
+		return errors.New("could not read filename")
 	}
 
-	q.Filename = strings.TrimRight(q.Filename, "\x00") // remove the 0-byte
-	if len(q.Filename) == 0 {
-		return errors.New("invalid RRQ")
+	w.Filename = strings.TrimRight(w.Filename, "\x00")
+	if len(w.Filename) == 0 {
+		return errors.New("invalid WRQ, filename is empty")
 	}
 
-	mode, err := r.ReadString(0) // read mode
+	mode, err := buf.ReadString(0)
 	if err != nil {
-		return errors.New("invalid RRQ")
+		return errors.New(fmt.Sprintf("invalid WRQ: could not read mode, due to err, %#v", err))
 	}
 
-	mode = strings.TrimRight(mode, "\x00") // remove the 0-byte
-	if len(q.Mode) == 0 {
-		return errors.New("invalid RRQ")
+	mode = strings.TrimRight(mode, "\x00")
+	if len(mode) == 0 {
+		return errors.New("invalid WRQ, mode is empty upon trimming")
 	}
 
-	q.Mode = tftp.TransferMode(strings.ToLower(mode))
-	if q.Mode != tftp.Octet {
+	w.Mode = tftp.TransferMode(strings.ToLower(mode))
+	if w.Mode != tftp.Octet {
 		return errors.New("only binary transfers supported")
 	}
+
+	fmt.Printf("%#v", w)
 
 	return nil
 }
